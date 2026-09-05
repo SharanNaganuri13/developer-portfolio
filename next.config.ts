c4ab126f299d684d5025ad7e5956ee894e4b43e2
Sharan Naganuri <isharanu.077@gmail.com>
2026-09-05 15:59:47 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   agentRules: false,
   output: 'export',
+  basePath: isProduction ? '/developer-portfolio' : '',
   images: {
     unoptimized: true,
   },
