d7e75704c7731ff816f9825cc591f87b9f7e236a
Sharan Naganuri <isharanu.077@gmail.com>
2026-09-05 15:36:30 +0530

    Configured Next.js

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,10 @@ const nextConfig: NextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   agentRules: false,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
